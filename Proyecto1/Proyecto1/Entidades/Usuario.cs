98f3b0b54ded3de7840ec4d3dd79c48dceb65da6
martinbaudin <martinbaudino11@gmail.com>
2022-08-24 11:43:25 -0300

    Subida de Archivos

---
+++ Proyecto1/Proyecto1/Entidades/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1.Entidades
+{
+    private class Usuario
+    {
+
+    }
+}
