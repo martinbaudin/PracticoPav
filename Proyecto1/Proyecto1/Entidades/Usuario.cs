2227fb1d44c04b1660d82d007cbff3e66733e840
martinbaudin <martinbaudino11@gmail.com>
2022-08-24 12:38:06 -0300

    Proyecto clase 2

--- Proyecto1/Proyecto1/Entidades/Usuario.cs
+++ Proyecto1/Proyecto1/Entidades/Usuario.cs
@@ -6,8 +6,41 @@ using System.Threading.Tasks;
 
 namespace Proyecto1.Entidades
 {
-    private class Usuario
+    internal class Usuario
     {
+        //Propiedad No Abreviada
+        private string nombre;
+        private string password;
 
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        public Usuario()
+        {
+            nombre = password = string.Empty;
+        }
+        public Usuario(string nombre, string password)
+        {
+            this.nombre = nombre;
+            this.password = password;
+        }
+        public bool validar()
+        {
+            string consultaSQL = "SELECT * FROM Usuarios WHERE usuario='"
+                                 + this.Nombre + "' AND password='"
+                                 + this.Password + "'";
+            DBHelper oDB = new DBHelper();
+            if (oDB.consultarDB(consultaSQL).Rows.Count == 0)
+                return false;
+            else
+                return true;
+        }
     }
 }
---
+++ Proyecto1/Proyecto1/RepositoriosBD/DBHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Proyecto1.RepositoriosBD
+{
+    internal class DBHelper
+    {
+        SqlConnection conexion;
+        SqlCommand comando;
+        //string cadenaConexion = @"Data Source=Maquis;Initial Catalog=BugTracker;User ID=avisuales1;password=Pav1#2020Maquis";
+        string cadenaConexion = @"Data Source=CX-OSCAR\SQLEXPRESS;Initial Catalog=BTS;Integrated Security=True";
+
+        public DataTable consultarDB(string consultaSQL)
+        {
+            conexion = new SqlConnection();
+            conexion.ConnectionString = cadenaConexion;
+            conexion.Open();
+
+            comando = new SqlCommand();
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = consultaSQL;
+
+            DataTable tabla = new DataTable();
+            tabla.Load(comando.ExecuteReader());
+
+            conexion.Close();
+
+            return tabla;
+        }
+    }
+}
